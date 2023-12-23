546a497fb2fe87fa014eb62b4eb30e72235678be
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-23 15:42:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,88 @@
 import streamlit as st
+import openai
+from pytube import YouTube
 import os
 from pathlib import Path
 from zipfile import ZipFile
-import openai
-# Make sure to replace 'whisper' with the actual library you are using for loading the model
-import whisper  
+import whisper
 
+# Load OpenAI API key from the Streamlit sidebar
+openai.api_key = st.sidebar.text_input('Enter OpenAI API Key', type='password')
 
-# Set the OpenAI API key
-api_key = st.sidebar.text_input('Enter your OpenAI API key:', type='password')
+@st.cache
+def load_model():
+    model = whisper.load_model("base")
+    return model
 
-# Check if API key is provided
-if not api_key:
-    st.sidebar.warning('Please enter your OpenAI API key in the sidebar.')
-else:
-    openai.api_key = api_key  # Set the API key for OpenAI
+def save_audio(url):
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download()
+    base, ext = os.path.splitext(out_file)
+    file_name = base + '.mp3'
+    try:
+        os.rename(out_file, file_name)
+    except WindowsError:
+        os.remove(file_name)
+        os.rename(out_file, file_name)
+    audio_filename = Path(file_name).stem+'.mp3'
+    print(yt.title + " Has been successfully downloaded")
+    print(file_name)
+    return yt.title, audio_filename
 
-    @st.cache
-    def load_model():
-        # Replace 'whisper' with the actual library you are using for loading the model
-        model = whisper.load_model("base")
-        return model
+def audio_to_transcript(audio_file):
+    model = load_model()
+    result = model.transcribe(audio_file)
+    transcript = result["text"]
+    return transcript
 
-    def save_audio(url):
-        yt = YouTube(url)
-        video = yt.streams.filter(only_audio=True).first()
-        out_file = video.download()
-        base, ext = os.path.splitext(out_file)
-        file_name = base + '.mp3'
-        try:
-            os.rename(out_file, file_name)
-        except WindowsError:
-            os.remove(file_name)
-            os.rename(out_file, file_name)
-        audio_filename = Path(file_name).stem + '.mp3'
-        st.success(yt.title() + " has been successfully downloaded")  # Fix: Use yt.title()
-        return yt.title(), audio_filename  # Fix: Use yt.title()
+def text_to_news_article(text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Write a news article in 500 words from the below text:\n"+text,
+        temperature=0.7,
+        max_tokens=600,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response['choices'][0]['text']
 
-    def audio_to_transcript(audio_file):
-        model = load_model()
-        result = model.transcribe(audio_file)
-        transcript = result["text"]
-        return transcript
+st.sidebar.markdown('# 🤖 **OpenAI GPT API Key**')
+st.markdown('# 📝 **News Article Generator App**')
 
-    def text_to_news_article(text):
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt="Write a news article in 500 words from the below text:\n" + text,
-            temperature=0.7,
-            max_tokens=600,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
+st.header('Input the Video URL')
+
+url_link = st.text_input('Enter URL of YouTube video:')
+
+if st.checkbox('Start Analysis'):
+    video_title, audio_filename = save_audio(url_link)
+    st.audio(audio_filename)
+    transcript = audio_to_transcript(audio_filename)
+    st.header("Transcript are getting generated...")
+    st.success(transcript)
+    st.header("News Article")
+    result = text_to_news_article(transcript)
+    st.success(result)
+    
+    # Save the files
+    transcript_txt = open('transcript.txt', 'w')
+    transcript_txt.write(transcript)
+    transcript_txt.close()  
+    
+    article_txt = open('article.txt', 'w')
+    article_txt.write(result) 
+    article_txt.close() 
+    
+    zip_file = ZipFile('output.zip', 'w')
+    zip_file.write('transcript.txt')
+    zip_file.write('article.txt')
+    zip_file.close()
+    
+    with open("output.zip", "rb") as zip_download:
+        btn = st.download_button(
+            label="Download ZIP",
+            data=zip_download,
+            file_name="output.zip",
+            mime="application/zip"
         )
-        return response['choices'][0]['text']
-
-    st.markdown('# 📝 **News Article Generator App**')
-
-    st.sidebar.header('API Configuration')
-    st.sidebar.info("Provide your OpenAI API key to use the app.")
-
-    st.header('Input the Video URL')
-    url_link = st.text_input('Enter URL of YouTube video:')
-
-    if st.checkbox('Start Analysis'):
-        video_title, audio_filename = save_audio(url_link)
-        st.audio(audio_filename)
-        transcript = audio_to_transcript(audio_filename)
-        st.header("Transcript is getting generated...")
-        st.success(transcript)
-        st.header("News Article")
-        result = text_to_news_article(transcript)
-        st.success(result)
-
-        # Save the files
-        transcript_txt = open('transcript.txt', 'w')
-        transcript_txt.write(transcript)
-        transcript_txt.close()
-
-        article_txt = open('article.txt', 'w')
-        article_txt.write(result)
-        article_txt.close()
-
-        zip_file = ZipFile('output.zip', 'w')
-        zip_file.write('transcript.txt')
-        zip_file.write('article.txt')
-        zip_file.close()
-
-        with open("output.zip", "rb") as zip_download:
-            btn = st.download_button(
-                label="Download ZIP",
-                data=zip_download,
-                file_name="output.zip",
-                mime="application/zip"
-            )
