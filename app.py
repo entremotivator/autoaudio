515aaff4be3ffcd9c1a3f3f55fde2cb5d004f47b
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-24 03:09:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pyaudio
 import numpy as np
+import wave
+import io
+import base64
 
 def main():
-    st.title("Streamlit App with PyAudio")
+    st.title("Streamlit Audio Recorder")
 
     # Function to capture audio and display a plot
     def capture_audio():
@@ -16,7 +19,7 @@ def main():
                         input=True,
                         frames_per_buffer=1024)
 
-        st.write("Listening...")
+        st.write("Recording...")
 
         # Collect audio data for a few seconds
         frames = []
@@ -29,8 +32,30 @@ def main():
         stream.close()
         p.terminate()
 
+        st.write("Recording complete!")
+
         # Display audio plot
         st.line_chart(np.concatenate(frames, axis=0))
+
+        # Save the recorded audio to a WAV file
+        save_audio(frames)
+
+    # Function to save recorded audio to a WAV file
+    def save_audio(frames):
+        p = pyaudio.PyAudio()
+        wf = wave.open("recorded_audio.wav", "wb")
+        wf.setnchannels(1)
+        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(44100)
+        wf.writeframes(b"".join(frames))
+        wf.close()
+
+        st.write("Audio saved to recorded_audio.wav")
+
+        # Provide a download link for the saved audio file
+        with open("recorded_audio.wav", "rb") as audio_file:
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format="audio/wav", start_time=0)
 
     # Button to start capturing audio
     if st.button("Start Audio Capture"):
